589bca79305606bbe97c732bbb707cb721dc82bf
Sumanth Ratna <sumanthratna@gmail.com>
2020-09-23 12:59:29 -0400

    Update multichat/urls.py

--- multichat/multichat/urls.py
+++ multichat/multichat/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from chat.views import index
 
 
 urlpatterns = [
     path('', index),
-    path('accounts/login/', login),
-    path('accounts/logout/', logout),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
